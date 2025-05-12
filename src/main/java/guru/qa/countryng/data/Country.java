6763e6487bb4fbbb74acead2bcf7c70d38ecc9e9
Anton Yakutovich <drakulavich@gmail.com>
2025-05-12 21:18:26 +0400

    Extend Entity with lastModifyDate

--- src/main/java/guru/qa/countryng/data/Country.java
+++ src/main/java/guru/qa/countryng/data/Country.java
@@ -2,21 +2,27 @@ package guru.qa.countryng.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalDateTime;
+
 public record Country(@JsonProperty("country_name")
                       String countryName,
                       @JsonProperty("country_code")
-                      String countryCode) {
+                      String countryCode,
+                      @JsonProperty("last_modify_date")
+                      LocalDateTime lastModifyDate) {
 
     public static Country fromEntity(CountryEntity ce) {
         return new Country(
                 ce.getCountryName(),
-                ce.getCountryCode());
+                ce.getCountryCode(),
+                ce.getLastModifyDate());
     }
 
     public CountryEntity toEntity() {
         CountryEntity entity = new CountryEntity();
         entity.setCountryName(this.countryName);
         entity.setCountryCode(this.countryCode);
+        entity.setLastModifyDate(this.lastModifyDate);
         return entity;
     }
 }
