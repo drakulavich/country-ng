957f80a22126df7b6346ce7c5166c9e51c2e7c41
Anton Yakutovich <drakulavich@gmail.com>
2025-05-22 21:57:21 +0400

    Implement gRPC service * Add country with streaming * Get country by id * Get all countries * Update country by id

--- src/main/java/guru/qa/countryng/data/Country.java
+++ src/main/java/guru/qa/countryng/data/Country.java
@@ -3,16 +3,21 @@ package guru.qa.countryng.data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record Country(@JsonProperty("country_name")
-                      String countryName,
-                      @JsonProperty("country_code")
-                      String countryCode,
-                      @JsonProperty("last_modify_date")
-                      LocalDateTime lastModifyDate) {
+public record Country(
+        @JsonProperty("id")
+        UUID id,
+        @JsonProperty("country_name")
+        String countryName,
+        @JsonProperty("country_code")
+        String countryCode,
+        @JsonProperty("last_modify_date")
+        LocalDateTime lastModifyDate) {
 
     public static Country fromEntity(CountryEntity ce) {
         return new Country(
+                ce.getId(),
                 ce.getCountryName(),
                 ce.getCountryCode(),
                 ce.getLastModifyDate());
